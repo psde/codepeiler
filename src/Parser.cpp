cd4a20536e73189d84df96566be86a3053b4ac80
Mathias Garbe <me@mathias-garbe.de>
2011-05-25 22:56:42 +0200

    More work on the parser, first prototype of grammar rule managment

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -1,9 +1,24 @@
 #include "Parser.hpp"
 
+#include <iostream>
+
+// TODO: Dear science, C is fugly as hell. Beautify this, asap.
+int SIZE_RULE_PROGSTART = 7;
+Token::TType RULE_PROGSTART[] =
+{
+    Token::TOKEN_PRINT,
+    Token::TOKEN_READ,
+    Token::TOKEN_INT,
+    Token::TOKEN_IDENTIFIER,
+    Token::TOKEN_IF,
+    Token::TOKEN_WHILE,
+    Token::TOKEN_EOF
+};
+
 Parser::Parser(Lexer *lexer)
  : lexer(lexer)
 {
-    
+    this->setup();    
 }
 
 Parser::~Parser()
@@ -11,9 +26,25 @@ Parser::~Parser()
 
 }
 
+bool Parser::isTokenInRule(Token tok, Token::TType *rule, int size)
+{
+    for(int i=0; i<size; i++)
+    {
+        if(tok.getType() == rule[i])
+            return true;
+    }
+    return false;
+}
+
+void Parser::setup()
+{
+    
+}
+
 void Parser::nextToken()
 {
     this->currentToken = this->lexer->nextToken();
+    std::cout << "Next token is: " << this->currentToken.getTokenDescription() << std::endl;
 }
 
 ParseTree *Parser::parse()
@@ -23,6 +54,23 @@ ParseTree *Parser::parse()
 }
 
 ParseTree *Parser::parseProg()
+{ 
+    if(this->isTokenInRule(currentToken, RULE_PROGSTART, SIZE_RULE_PROGSTART))
+    {
+        std::cout << "Seems like it is in RULE_PROGSTART..." << std::endl;
+        this->parseDecls();
+        this->parseStatements();
+    }
+
+    return new ParseTree();
+}
+
+ParseTree *Parser::parseDecls()
+{
+    return new ParseTree();
+}
+
+ParseTree *Parser::parseStatements()
 {
     return new ParseTree();
 }
--- src/Parser.hpp
+++ src/Parser.hpp
@@ -1,16 +1,28 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include "Token.hpp"
 #include "Lexer.hpp"
 #include "ParseTree.hpp"
+#include "List.hpp"
+#include "Token.hpp"
 
 class Parser
 {
 private:
 
+    /*enum RuleTypes {
+        RULE_PROGSTART = 0,
+        RULE_NO
+    };
+    List<Token::TType> rules[Parser::RULE_NO];*/
+
     Lexer *lexer;
     Token currentToken;
     void nextToken();
+
+    bool isTokenInRule(Token tok, Token::TType *rule, int size);
+    void setup();
 
     ParseTree *parseProg();
     ParseTree *parseDecls();
