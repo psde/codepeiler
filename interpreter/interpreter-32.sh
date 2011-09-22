c32bce8b7deee0ffabe55a4f5ee3d7945b06f243
Mathias Garbe <me@mathias-garbe.de>
2011-09-22 15:28:08 +0200

    Added shellscripts to start the interpreter without copying the lib (We don't want that stinkin' lib pollute our system..)

---
+++ interpreter/interpreter-32.sh
@@ -0,0 +1,1 @@
+java -Djava.library.path="/home/mathias/devel/codepeiler/interpreter/i386/" -jar interpreter.jar
---
+++ interpreter/interpreter-64.sh
@@ -0,0 +1,1 @@
+java -Djava.library.path="/home/mathias/devel/codepeiler/interpreter/x86_64/" -jar interpreter.jar
