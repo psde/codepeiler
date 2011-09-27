f2e3b58674fa5a52868b4811d7768e1c3652a354
Mathias Garbe <me@mathias-garbe.de>
2011-09-27 20:12:01 +0200

    String can now append int

--- src/String.hpp
+++ src/String.hpp
@@ -133,6 +133,26 @@ class String
             return str;
         }
 
+        void operator+=(int const& l)
+        {
+            char buf[sizeof(int)*8+1];
+            sprintf(buf, "%i", l);
+
+            unsigned int length = stringLength(this->chars) + stringLength(buf) + 1;
+            char* newChars = new char[length];
+            
+            strcpy(newChars, this->chars);
+            newChars[length-1] = 0x00;
+
+            for(unsigned int i=stringLength(this->chars); i<length-1; i++)
+            {
+                newChars[i] = buf[i-stringLength(this->chars)];
+            }
+
+            delete[] this->chars;
+            this->chars = newChars;
+        }
+
         /*
           Appends a long
         */
